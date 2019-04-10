fc0c3cf4cc1c2b024b48941c748a5f78e8cd28a0
Victoria Arias <Victoria Arias@Victoria>
2019-04-09 22:51:33 -0600

    Victoria

--- Proyecto/src/proyecto/Proyecto.java
+++ Proyecto/src/proyecto/Proyecto.java
@@ -17,6 +17,7 @@ public class Proyecto {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
         System.out.println("Hola Victorious");
+        System.out.println("Hola JP");
     }
     
 }
