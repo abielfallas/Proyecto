eeaea59a2b229256b0fe25d17e1ff0185c3d424d
Abiel Fallas <Abiel Fallas@LAPTOP-VHI7570C>
2019-04-10 15:01:15 -0600

    Abiel

--- Proyecto/src/proyecto/Proyecto.java
+++ Proyecto/src/proyecto/Proyecto.java
@@ -17,14 +17,7 @@ public class Proyecto {
      */
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
-        System.out.println("Hola Victorious");
-        System.out.println("Hola JP");
-        System.out.println("A Karina se le fue el internet :(");
-        System.out.println("Parece que si se le fue xD");
-        System.out.println("Desde VersionS");
-        System.out.println("tembloooo");
-        System.out.println("Abiel");
-        System.out.println("KKKKK");
+
 
     }
     
