a4f653cabacf89b42eae3115f4352bd6962ac90c
sugei <sugei@LAPTOP-BBA5NVFP>
2019-04-10 16:40:35 -0600

    Sugeilyn

--- Proyecto/src/Clases/Pacientes.java
+++ Proyecto/src/Clases/Pacientes.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Clases;
 
 /**
@@ -10,5 +6,5 @@ package Clases;
  * @author Abiel Fallas
  */
 public class Pacientes {
-    
+     
 }
